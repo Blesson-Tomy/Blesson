5e531a6620acc1df3e59e298dbd59b8720ee1428
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-05-09 23:55:57 +0530

    fixed responsiveness

--- Script.js
+++ Script.js
@@ -1,13 +1,6 @@
 function submitForm()
 {
-    var form = document.getElementById("form").submit();
+    var form = document.getElementById("myForm").submit();
     google.script.run.sendForm(form);
 }
 
-function sendForm(form)
-{
-    var emailAddress = "blessonkarikulam@gmail.com";
-    var subject = "New Form Submission";
-    var message = "Name: " + formData.name + "\nEmail: " + formData.email + "\nMessage: " + formData.message;
-    MailApp.sendEmail(emailAddress, subject, message);
-  }
